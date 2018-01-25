07922780b84dbb68d8e70193aed55f732076039a
Jay Park <jay.jongcheol.park@gmail.com>
2018-01-25 17:47:16 -0500

    Add Close button for deleting post

--- frontend/src/components/Post/Post.js
+++ frontend/src/components/Post/Post.js
@@ -5,6 +5,15 @@ import TextTruncate from 'react-text-truncate'
 import { upVoteToPost, downVoteToPost } from '../../redux/actions'
 
 class Post extends Component {
+  state = {
+    hover: false
+  }
+
+  toggleHover = () => {
+    this.setState((prevState) => ({
+      hover: !prevState.hover
+    }))
+  }
 
   upVote = () => {
     this.props.upVoteToPost(this.props.post.id)
@@ -14,26 +23,48 @@ class Post extends Component {
     this.props.downVoteToPost(this.props.post.id)
   }
 
-  render () {
+  render() {
     const { post } = this.props
     if (!post) {
       return <div />
     }
-    const date = new Date(post.timestamp);
-    const hot = (post.voteScore >= 10) ? <span class="badge badge-danger">HOT</span> : <span />
+
+    let closeStyle
+    if (this.state.hover) {
+      closeStyle = 'text-danger'
+    } else {
+      closeStyle = 'text-muted'
+    }
+
+    const date = new Date(post.timestamp)
+    const hot =
+      post.voteScore >= 10 ? (
+        <span className="badge badge-danger">HOT</span>
+      ) : (
+        <span />
+      )
     return (
       <li className="list-group-item">
-        <h2>{post.title}{' '}{hot}</h2>
+        <div className="d-flex justify-content-between align-items-start">
+          <h2>
+            {post.title} {hot}
+          </h2>
+          <button
+            type="button"
+            className={`close ${closeStyle}`}
+            aria-label="Close"
+            onMouseEnter={this.toggleHover}
+            onMouseLeave={this.toggleHover}
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
         <div>
           <small className="text-muted">Posted by {post.author}</small>
         </div>
-        <p className="h5 mt-4">
-          <TextTruncate
-            line={1}
-            truncateText="..."
-            text={post.body}
-          />
-        </p>
+        <div className="mt-4">
+          <TextTruncate line={1} truncateText="..." text={post.body} />
+        </div>
         <div>
           <small className="text-muted">{date.toString()}</small>
         </div>
@@ -43,14 +74,22 @@ class Post extends Component {
               {_.capitalize(post.category)}
             </span>
             <span className="badge badge-secondary" disabled>
-              Vote{' '}<span className="text-warning">{post.voteScore}</span>
+              Vote <span className="text-warning">{post.voteScore}</span>
             </span>
           </div>
           <div>
-            <button onClick={this.upVote} className="btn btn-outline-primary mr-3">
-            <i className="fa fa-thumbs-o-up" aria-hidden="true"></i></button>
-            <button onClick={this.downVote} className="btn btn-outline-secondary">
-            <i className="fa fa-thumbs-o-down" aria-hidden="true"></i></button>
+            <button
+              onClick={this.upVote}
+              className="btn btn-outline-primary mr-3"
+            >
+              <i className="fa fa-thumbs-o-up" aria-hidden="true" />
+            </button>
+            <button
+              onClick={this.downVote}
+              className="btn btn-outline-secondary"
+            >
+              <i className="fa fa-thumbs-o-down" aria-hidden="true" />
+            </button>
           </div>
         </div>
       </li>
@@ -58,6 +97,4 @@ class Post extends Component {
   }
 }
 
-export default connect(
-  null, { upVoteToPost, downVoteToPost }
-)(Post)
+export default connect(null, { upVoteToPost, downVoteToPost })(Post)
