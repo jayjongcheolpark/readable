3c1917c0635026b4bb0644f075f9b4a050311a34
Jay Park <jay.jongcheol.park@gmail.com>
2018-01-24 23:24:42 -0500

    Add unified event handler to PostForm

--- frontend/src/components/PostForm/PostForm.js
+++ frontend/src/components/PostForm/PostForm.js
@@ -3,13 +3,20 @@ import _ from 'lodash'
 
 class PostForm extends Component {
   state = {
-    category: "react"
+    category: "react",
+    title: "",
+    content: "",
+    author: ""
   }
 
-  handleOptionChange = (changeEvent) => {
+  handleChange= (e, key) => {
     this.setState({
-      category: changeEvent.target.value
+      [key]: e.target.value
     })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
   }
 
   render() {
@@ -27,7 +34,7 @@ class PostForm extends Component {
               <input
                 type="radio" name="categories" id={category}
                 autoComplete="off" value={category}
-                onChange={this.handleOptionChange}
+                onChange={(e) => this.handleChange(e, "category")}
                 checked={this.state.category === category}
               />
               {_.capitalize(category)}
@@ -38,7 +45,7 @@ class PostForm extends Component {
 
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label className="d-block">Category</label>
           <div
             className="btn-group btn-group-toggle mb-4"
@@ -50,15 +57,27 @@ class PostForm extends Component {
           </div>
           <div className="form-group">
             <label htmlFor="inputTitle">Title</label>
-            <input type="text" className="form-control" id="inputTitle" />
+            <input
+              type="text" className="form-control" id="inputTitle"
+              value={this.state.title} required
+              onChange={(e) => this.handleChange(e, "title")}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="inputContent">Content</label>
-            <textarea type="text" className="form-control" id="inputContent" />
+            <textarea
+              type="text" className="form-control" id="inputContent"
+              value={this.state.content} required
+              onChange={(e) => this.handleChange(e, "content")}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="inputAuthor">Author</label>
-            <input type="text" className="form-control" id="inputAuthor" />
+            <input
+              type="text" className="form-control" id="inputAuthor"
+              value={this.state.author} required
+              onChange={(e) => this.handleChange(e, "author")}
+            />
           </div>
           <div className="mt-5">
             <button type="submit" className="btn btn-lg btn-primary btn-block">
