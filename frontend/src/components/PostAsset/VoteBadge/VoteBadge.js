8f59ea853e33df0414192aa299c0a5f8517be3eb
Jay Park <jay.jongcheol.park@gmail.com>
2018-01-27 22:26:40 -0500

    Change badgeColor when vote >= 10

--- frontend/src/components/PostAsset/VoteBadge/VoteBadge.js
+++ frontend/src/components/PostAsset/VoteBadge/VoteBadge.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
 const VoteBadge = ({voteScore}) => {
+  let badgeColor = "badge-secondary"
+  if (voteScore >=10) {
+    badgeColor = "badge-danger"
+  }
   return (
-    <span className="badge badge-secondary" disabled>
+    <span className={`badge ${badgeColor}`} disabled>
       Vote{' '}
       <span className="text-warning">
         {voteScore}
