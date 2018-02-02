75473ff5d45c46cda95e83e3d429fbf99a73b91a
Jay Park <jay.jongcheol.park@gmail.com>
2018-02-01 19:32:18 -0500

    Add voteBadge and up down button

--- frontend/src/components/CommentAsset/Comment/Comment.js
+++ frontend/src/components/CommentAsset/Comment/Comment.js
@@ -1,13 +1,34 @@
 import React from 'react'
+import VoteBadge from '../../PostAsset/VoteBadge/VoteBadge'
+import IconButton from '../../PostAsset/IconButton/IconButton'
 
 const Comment = ({ comment }) => {
   return (
     <li className="list-group-item list-group-item-warning">
       <div>{comment.body}</div>
-      <small className="text-muted">commented by {comment.author}</small>{' / '}
-      <small className="text-muted">
-        {(new Date(comment.timestamp)).toLocaleString('en-CA',{ timeZone: 'America/Toronto'})}
-      </small>
+      <div>
+        <small className="text-muted">commented by {comment.author}</small>
+        {' / '}
+        <small className="text-muted">
+          {(new Date(comment.timestamp))
+            .toLocaleString('en-CA',{ timeZone: 'America/Toronto'})}
+        </small>
+      </div>
+      <div className="d-flex justify-content-between align-items-end">
+        <VoteBadge voteScore={comment.voteScore} />
+        <div>
+          <IconButton
+            buttonClass="btn btn-outline-primary mr-3"
+            iconClass="fa fa-thumbs-o-up"
+            clickHandler={() => comment.voteScore}
+          />
+          <IconButton
+            buttonClass="btn btn-outline-secondary"
+            iconClass="fa fa-thumbs-o-down"
+            clickHandler={() => comment.voteScore}
+          />
+      </div>
+      </div>
     </li>
   )
 }
