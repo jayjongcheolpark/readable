683b3d3cb41120a641c0fcf2168b481b749719c3
Jay Park <jay.jongcheol.park@gmail.com>
2018-02-04 21:29:20 -0500

    Add checking authorization

--- api-server/server.js
+++ api-server/server.js
@@ -127,8 +127,20 @@ app.use(cors())
 //   }
 // })
 
+function ensureAuthenticated(req, res, next) {
+  const token = req.get('Authorization')
 
-app.get('/categories', (req, res) => {
+  if (token) {
+    req.token = token
+    next()
+  } else {
+  res.status(403).send({
+      error: 'Please provide an Authorization header to identify yourself (can be whatever you want)'
+  })
+}
+
+
+app.get('/categories', ensureAuthenticated, (req, res) => {
     categories.getAll(req.token)
       .then(
           (data) => res.send(data),
@@ -141,7 +153,7 @@ app.get('/categories', (req, res) => {
       )
 })
 
-app.get('/:category/posts', (req, res) => {
+app.get('/:category/posts', ensureAuthenticated, (req, res) => {
     posts.getByCategory(req.token, req.params.category)
       .then(
           (data) => res.send(data),
@@ -154,7 +166,7 @@ app.get('/:category/posts', (req, res) => {
       )
 })
 
-app.get('/posts', (req, res) => {
+app.get('/posts', ensureAuthenticated, (req, res) => {
     posts.getAll(req.token)
       .then(
           (data) => res.send(data),
@@ -167,7 +179,7 @@ app.get('/posts', (req, res) => {
       )
 })
 
-app.post('/posts', bodyParser.json(), (req, res) => {
+app.post('/posts', ensureAuthenticated, bodyParser.json(), (req, res) => {
     posts.add(req.token, req.body)
       .then(
           (data) => res.send(data),
@@ -180,7 +192,7 @@ app.post('/posts', bodyParser.json(), (req, res) => {
       )
 })
 
-app.get('/posts/:id', (req, res) => {
+app.get('/posts/:id', ensureAuthenticated, (req, res) => {
     posts.get(req.token, req.params.id)
       .then(
           (data) => res.send(data),
@@ -193,7 +205,7 @@ app.get('/posts/:id', (req, res) => {
       )
 })
 
-app.delete('/posts/:id', (req, res) => {
+app.delete('/posts/:id', ensureAuthenticated, (req, res) => {
     posts.disable(req.token, req.params.id)
       .then(post => comments.disableByParent(req.token, post))
       .then(
@@ -207,7 +219,7 @@ app.delete('/posts/:id', (req, res) => {
       )
 })
 
-app.post('/posts/:id', bodyParser.json(), (req, res) => {
+app.post('/posts/:id', ensureAuthenticated, bodyParser.json(), (req, res) => {
     const { option } = req.body
     const id = req.params.id
     posts.vote(req.token, id, option)
@@ -222,7 +234,7 @@ app.post('/posts/:id', bodyParser.json(), (req, res) => {
       )
 })
 
-app.put('/posts/:id', bodyParser.json(), (req, res) => {
+app.put('/posts/:id', ensureAuthenticated, bodyParser.json(), (req, res) => {
     posts.edit(req.token, req.params.id, req.body)
       .then(
         (data) => res.send(data),
@@ -235,7 +247,7 @@ app.put('/posts/:id', bodyParser.json(), (req, res) => {
       )
 })
 
-app.get('/posts/:id/comments', (req, res) => {
+app.get('/posts/:id/comments', ensureAuthenticated, (req, res) => {
     comments.getByParent(req.token, req.params.id)
       .then(
           (data) => res.send(data),
@@ -248,7 +260,7 @@ app.get('/posts/:id/comments', (req, res) => {
       )
 })
 
-app.get('/comments/:id', (req, res) => {
+app.get('/comments/:id', ensureAuthenticated, (req, res) => {
     comments.get(req.token, req.params.id)
       .then(
           (data) => res.send(data),
@@ -261,7 +273,7 @@ app.get('/comments/:id', (req, res) => {
       )
 })
 
-app.put('/comments/:id', bodyParser.json(), (req, res) => {
+app.put('/comments/:id', ensureAuthenticated, bodyParser.json(), (req, res) => {
     comments.edit(req.token, req.params.id, req.body)
       .then(
         (data) => res.send(data),
@@ -274,7 +286,7 @@ app.put('/comments/:id', bodyParser.json(), (req, res) => {
       )
 })
 
-app.post('/comments', bodyParser.json(), (req, res) => {
+app.post('/comments', ensureAuthenticated, bodyParser.json(), (req, res) => {
     comments.add(req.token, req.body)
       .then(
           (data) => res.send(data),
@@ -287,7 +299,7 @@ app.post('/comments', bodyParser.json(), (req, res) => {
       )
 })
 
-app.post('/comments/:id', bodyParser.json(), (req, res) => {
+app.post('/comments/:id', ensureAuthenticated, bodyParser.json(), (req, res) => {
     const { option } = req.body
     comments.vote(req.token, req.params.id, option)
       .then(
@@ -301,7 +313,7 @@ app.post('/comments/:id', bodyParser.json(), (req, res) => {
       )
 })
 
-app.delete('/comments/:id', (req, res) => {
+app.delete('/comments/:id', ensureAuthenticated, (req, res) => {
     comments.disable(req.token, req.params.id)
       .then(
           (data) => res.send(data),
